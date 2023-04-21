dffea5689be5097f22b13b0a1ccaef2ecf04a684
Subodh <subodhjajee123@gmail.com>
2023-04-21 11:02:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { FaGoogle, FaGithub, FaFacebook } from "react-icons/fa";
 
 function App() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSignUp = () => {
+    setIsSignUp(true);
+  };
+
+  const handleSignIn = () => {
+    setIsSignUp(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`container ${isSignUp ? 'right-panel-active' : ''}`}>
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h1>Create Account</h1>
+            <div className="social-container">
+              <a href="#" className="social"><FaGoogle /></a>
+              <a href="#" className="social"><FaGithub /></a>
+              <a href="#" className="social"><FaFacebook /></a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign in</h1>
+            <div className="social-container">
+              <a href="#" className="social"><FaGoogle /></a>
+              <a href="#" className="social"><FaGithub /></a>
+              <a href="#" className="social"><FaFacebook /></a>
+            </div>
+            <span>or use your account</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forgot your password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>To keep connected with us please login with your personal info</p>
+              <button className="ghost" onClick={handleSignIn}>Sign In</button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button className="ghost" onClick={handleSignUp}>Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
